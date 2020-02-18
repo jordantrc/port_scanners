a7a89a49e164af121c7e0f02bdf968f94cdd5ffb
Jordan Chadwick <jordan.chadwick@gmail.com>
2020-02-18 15:43:36 -0500

    fixes UDP port specification

--- masscan.sh
+++ masscan.sh
@@ -62,13 +62,13 @@ fi
 
 # set the port list for scanning
 tcp_default_portlist="1,3,7,9,13,17,19,21-23,25-26,37,53,79-82,88,100,106,110,111,113,119,135,139,143-144,179,199,254,255,280,311,389,\
-    427,443-445,464,465,497,513-515,543,544,548,554,587,593,625,631,636,646,787,808,873,902,993,995,999,1000,1022,\
-    1024-1033,1035-1041,1044,1048-1050,1053,1054,1056,1058,1059,1064-1066,1069,1071,1074,1080,1110,1234,1433,1494,1521,\
-    1720,1723,1755,1761,1801,1900,1935,1993,1998,2000-2002,2005,2049,2103,2105,2107,2121,2161,2301,2383,2401,2601,2717,\
-    2869,2967,3000,3001,3128,3268,3306,3389,3689,3690,3703,3986,4000,4001,4045,4899,5000,5001,5003,5009,5050,5051,5060,\
-    5101,5120,5190,5357,5432,5555,5556,5631,5666,5800,5900,5901,5985,6000-6002,6004,6112,6646,7000-7002,7070,7937,7938,8000-8002,\
-    8008-8010,8031,8080,8081,8443,8888,9000,9001,9090,9100,9102,9999,10000,10010,32768,32771,49152-49157,50000"
-udp_default_portlist="53,67,68,69,123,135,137-139,161,500,514,520,631,1434"
+427,443-445,464,465,497,513-515,543,544,548,554,587,593,625,631,636,646,787,808,873,902,993,995,999,1000,1022,\
+1024-1033,1035-1041,1044,1048-1050,1053,1054,1056,1058,1059,1064-1066,1069,1071,1074,1080,1110,1234,1433,1494,1521,\
+1720,1723,1755,1761,1801,1900,1935,1993,1998,2000-2002,2005,2049,2103,2105,2107,2121,2161,2301,2383,2401,2601,2717,\
+2869,2967,3000,3001,3128,3268,3306,3389,3689,3690,3703,3986,4000,4001,4045,4899,5000,5001,5003,5009,5050,5051,5060,\
+5101,5120,5190,5357,5432,5555,5556,5631,5666,5800,5900,5901,5985,6000-6002,6004,6112,6646,7000-7002,7070,7937,7938,8000-8002,\
+8008-8010,8031,8080,8081,8443,8888,9000,9001,9090,9100,9102,9999,10000,10010,32768,32771,49152-49157,50000"
+udp_default_portlist="53,67-69,123,135,137-139,161,500,514,520,631,1434"
 # these top port lists are obtained from the nmap port scanner
 # the file containing this service list (in Linux) is /usr/share/nmap/nmap-services
 # the lists below can be recreated with the following command:
@@ -99,7 +99,7 @@ then
         let end_range=num_ports-1
         for i in $(seq 0 $end_range); do
             tcp_portlist+="${tcp_top_100_portlist[$i]}"
-            udp_portlist+="${udp_top_100_portlist[$i]}"
+            udp_portlist+="U:${udp_top_100_portlist[$i]}"
             if [[ "$i" -lt "$end_range" ]]; then
                 tcp_portlist+=","
                 udp_portlist+=","
@@ -118,10 +118,12 @@ echo "TCP PORT LIST = $tcp_portlist"
 
 # check if UDP scan option is set
 if [ "$udp_scan" = true ]; then
-    udp_option="--udp-ports $udp_portlist"
     echo "UDP PORT LIST = $udp_portlist"
+    udp_portlist_formatted=$(echo "$udp_portlist" | sed 's/^/U:/; s/,/,U:/g')
+    port_argument="$udp_portlist_formatted,$tcp_portlist"
 else
     udp_option=""
+    port_argument="$tcp_portlist"
 fi
 
 # check if gw_mac_address option is set
@@ -131,7 +133,7 @@ else
     router_option=""
 fi
 
-masscan_cmd="masscan $target_specification --ping $router_option -p$tcp_portlist $udp_option --rate $rate -oL $outputbase.masscan"
+masscan_cmd="masscan $target_specification --ping $router_option -p$port_argument --rate $rate -oL $outputbase.masscan"
 
 if [ "$echo_only" = true ]; then
     echo "Masscan command:"
