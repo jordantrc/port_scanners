4d346850fd8b64d4aa6aea3adf5c37acee252346
Chadwick <jordan.chadwick@Fiserv.com>
2020-03-06 16:04:08 -0500

    working version

--- probe_services.sh
+++ probe_services.sh
@@ -20,19 +20,21 @@ echo "[*] using $pps_per_job pps/job"
 running_jobs=0
 for f in ${directory}/*; do
     filename=$(basename "$f")
+    echo "[*] processing $filename"
     proto=$(echo "$filename" | cut -d "_" -f 1)
-    port=$(echo "$filename" | cut -d "_" -f 2 | cut -d "." -f 1)    
+    port=$(echo "$filename" | cut -d "_" -f 2 | cut -d "." -f 1)
+    echo "[*] protocol = $proto, port = $port"   
 
     if [ ${proto} != "icmp" ]; then
-        num_hosts=$(cat $host_file | sort | uniq | wc -l) 
+        num_hosts=$(cat "$f" | sort | uniq | wc -l | sed 's/^ *//') 
         scan_file=$directory"/service_version_"$proto"_"$port".gnmap"
-        if [${proto} -eq "udp" ]; then
+        if [ ${proto} == "udp" ]; then
             scan_type_option="-sU"
         else
             scan_type_option=""
         fi
         # run each nmap task as background job so it can be done in parallel
-        nmap $scan_type_option --max-rate $pps_per_job -p $port -sV -Pn -iL $host_file -oG $scan_file &
+        nmap $scan_type_option --quiet --max-rate $pps_per_job -p $port -sV -Pn -iL "$f" -oG "$scan_file" &
         pid=$!
         running_jobs=$(expr $running_jobs + 1)
         echo "[*] started service detection job for $num_hosts $proto/$port hosts [PID $pid job $running_jobs of $num_jobs]"
