41362977025dfcf8e55efb4b1fbbb78a4aa53312
Jordan Chadwick <jordan.chadwick@gmail.com>
2020-08-27 09:34:47 -0400

    changes parse_line return

--- verify_and_report.py
+++ verify_and_report.py
@@ -41,7 +41,7 @@ def parse_scan_file(scan_file, output_directory):
                 else:
                     assert False, "file type unknown"
             port_info = parse_line(line, file_type)  # returns [state, proto, port, host, banner]
-            if port_info is not None and port_info[0] == "open":
+            if len(port_info) > 0 and port_info[0] == "open":
                 host_output(output_directory, port_info[1], port_info[2], port_info[3])
 
 
@@ -57,7 +57,7 @@ def produce_report(output_directory, report_file):
             with open(s, 'r') as s_fd:
                 for line in s_fd.readlines():
                     port_info = parse_line(line, "nmap")
-                    if port_info is not None:
+                    if len(port_info) > 0:
                         print("line = %s, port_info = %s" % (line, port_info))
                         csvwriter.writerow([port_info[3], port_info[1], port_info[2], port_info[0], port_info[4]])
 
@@ -66,7 +66,7 @@ def parse_line(line, file_type):
     """Parse a scan file line, returns state, proto, port, host, banner."""
     result = []
     if line[0] == "#":
-        return None
+        return result
 
     if file_type == "masscan":
         state, proto, port, host, _ = line.split()
